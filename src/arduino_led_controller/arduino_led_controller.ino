3571f1ee143e86b089abc2b334c3122b7cd4bef6
Marko Seifert <developerpodcast@marko-seifert.de>
2014-03-03 21:21:43 +0100

    LCD Lib integriert

--- src/arduino_led_controller/arduino_led_controller.ino
+++ src/arduino_led_controller/arduino_led_controller.ino
@@ -1,5 +1,3 @@
-
-
 /*
   Developer Podcast 
   Folge: DP001-Arduino_LED_Controller
@@ -8,10 +6,21 @@
   http://www.marko-seifert.de
   Apache 2 License
  
- */
- 
+*/
+
+#include <LiquidCrystal.h>
+
+LiquidCrystal* lcd ;
 
 void setup() {
+  lcd = new LiquidCrystal(8, 9, 4, 5, 6, 7);
+  lcd->clear();
+  lcd->begin(16, 2);
+  lcd->setCursor(0, 0);
+  lcd->print("DP001:");
+  lcd->setCursor(0, 1);
+  lcd->print("A-LED-Controller");
+
 
 }
 void loop() {
