e581c55f753c81d76f19d875cfcf164de93082c6
Marko Seifert <developerpodcast@marko-seifert.de>
2014-03-03 23:18:43 +0100

    ButtonValue behandelt

--- src/arduino_led_controller/arduino_led_controller.ino
+++ src/arduino_led_controller/arduino_led_controller.ino
@@ -11,6 +11,8 @@
 #include <LiquidCrystal.h>
 
 LiquidCrystal* lcd ;
+enum BtnKey {btnRIGHT, btnUP, btnDOWN, btnLEFT, btnSELECT, btnNONE};
+int oldkey = btnNONE;
 
 void setup() {
   lcd = new LiquidCrystal(8, 9, 4, 5, 6, 7);
@@ -23,6 +25,80 @@ void setup() {
 
 
 }
+
+void onButtonClickCallBack(unsigned int buttonEvent){
+  
+  lcd->clear();
+  lcd->setCursor(0, 1);
+
+  switch(buttonEvent){
+   case btnRIGHT:
+     lcd->print("btnRIGHT");
+     break;
+   case btnUP:
+     lcd->print("btnUP");
+     break; 
+   case btnDOWN: 
+     lcd->print("btnDOWN");
+     break;
+   case btnLEFT: 
+     lcd->print("btnLEFT");
+     break;
+   case btnSELECT: 
+     lcd->print("btnSELECT");
+     break;
+   case btnNONE:
+     lcd->print("btnNONE");
+     break;
+  }
+  
+
+  
+}
+void readButtonValue() {
+
+  int adc_key_in = analogRead(0);
+  
+  int key = transform(adc_key_in);
+  if (key != oldkey) {
+    delay(50);
+    adc_key_in = analogRead(0);
+    key = transform(adc_key_in);
+    if (key != oldkey) {
+      oldkey = key;
+      if (key != btnNONE) {
+        //debugEvent(adc_key_in);
+        onButtonClickCallBack(key);
+      }
+    }
+  }
+}
+void debugEvent(unsigned int event){
+  lcd->clear();
+  lcd->setCursor(0, 1);
+  char buffer[16];
+  sprintf(buffer, "Event: %d", event);
+  lcd->print(buffer);
+}
+int transform(unsigned int input) {
+  if (input > 1000)		
+    return btnNONE;
+  else if (input < 50)
+    return btnRIGHT;
+  else if (input < 195)
+    return btnUP;
+  else if (input < 380)
+    return btnDOWN;
+  else if (input < 555)
+    return btnLEFT;
+  else if (input < 790)
+    return btnSELECT;
+  else
+    return btnNONE;
+}
+
 void loop() {
+  
+  readButtonValue();
  
 }
